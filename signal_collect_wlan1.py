34d8aeeb5e53986fc35db9f61d597475885abc62
kalapalarajmohan <kalapalarajmohan@gmail.com>
2026-03-29 00:30:10 +0530

    wlan1 collector with wlan0 failover

--- signal_collect_wlan1.py
+++ signal_collect_wlan1.py
@@ -5,11 +5,17 @@ import datetime
 DATA = "/home/rajmohan/amma-project/data/amma_data_wlan1.txt"
 
 while True:
-    r = subprocess.run(['iwconfig','wlan1'],capture_output=True,text=True)
+    try:
+        r = subprocess.run(['iwconfig','wlan1'],capture_output=True,text=True)
+        if 'Signal level' not in r.stdout:
+            r = subprocess.run(['iwconfig','wlan0'],capture_output=True,text=True)
+    except:
+        r = subprocess.run(['iwconfig','wlan0'],capture_output=True,text=True)
     for line in r.stdout.split('\n'):
         if 'Signal level' in line:
             sig = line.split('Signal level=')[1].split(' ')[0]
             t = datetime.datetime.now().strftime('%H:%M:%S')
             with open(DATA,'a') as f:
-                f.write(t + ' ' +sig + '\n')
+                f.write(t + ' ' + sig +'\n')
     time.sleep(1)
+
