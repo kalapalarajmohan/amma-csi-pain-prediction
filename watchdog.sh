83daae08d4641f9e6c70b15d7ffdee187af389bf
kalapalarajmohan <kalapalarajmohan@gmail.com>
2026-03-28 23:50:33 +0530

    Dua WiFi collection - wlan0 and wlan1 running

--- watchdog.sh
+++ watchdog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pgrep -f signal_collect_v2.py || nohup python3.8 /home/rajmohan/amma-project/signal_collect_v2.py >> /home/rajmohan/amma-project/data/amma_data.txt 2>&1 & 
+pgrep -f signal_collect_wlan1.py || nohup python3.8 /home/rajmohan/amma-project/signal_collect_wlan1.py >> /home/rajmohan/amma-project/data/amma_data_wlan1.txt 2>&1 & 
 pgrep -f sentry.py || nohup python3.8 /home/rajmohan/amma-project/sentry.py >> /home/rajmohan/amma-project/data/alerts.txt 2>&1 &
