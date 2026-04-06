c406fe3cec7f4e6b57813154e9caed7ac060e7b2
kalapalarajmohan <kalapalarajmohan@gmail.com>
2026-04-06 08:03:44 +0530

    Add ESP32 CSI collector and systemd services

---
+++ esp32_collect.py
@@ -0,0 +1,13 @@
+import socket
+import datetime
+DATA= "/home/rajmohan/amma-project/data/esp32_data.txt"
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('',4210))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    sig = str(data)
+    t = datetime.datetime.now().strftime('%H:%M:%S')
+    with open(DATA, 'a') as f:
+        f.write(t + ' ' + sig + '\n')
+    print(t + ' ' + sig)
